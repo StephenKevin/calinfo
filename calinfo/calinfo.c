a7d7af59821b810d1b2496d87f96369e84ad53e0
StephenKwen <hyuncankun@outlook.com>
2018-03-17 11:51:14 +0800

    update

--- calinfo/calinfo.c
+++ calinfo/calinfo.c
@@ -1,12 +1,12 @@
-/** 学生信息统计
-功能:
-    1.添加一个记录
-    2.浏览全部信息
-    3.查询
-    4.统计
-    5.修改一个记录
-    6.删除一个记录
-    7.退出程序
+/** Calinfo
+Function:
+    1.Add
+    2.Browser all
+    3.Query
+    4.Statistics
+    5.Modify
+    6.Delete
+    7.Quit
 **/
 
 #ifndef CALINFO_C_
@@ -17,10 +17,10 @@
 
 int main(void)
 {
-    char menu_str[MENU_ST];     //储存用户输入
+    char menu_str[MENU_ST];     //store user's imput
 
-    table_main();               //打印主菜单
-    printf(">> ");              //输入指示符
+    table_main();               //print main menu
+    printf(">> ");              //print the indicator of input
 
     while(s_gets(menu_str, MENU_ST)!=NULL)
     {
